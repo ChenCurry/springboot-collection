456307c2f01dc35e4f9390510c113a3e6a427bc4
ChenCurry <chennyuuu@gmail.com>
2020-12-02 15:48:25 +0800

    wocao，教训

---
+++ sb-05-druid/src/main/java/com/hello/sb05druid/Sb05DruidApplication.java
@@ -0,0 +1,13 @@
+package com.hello.sb05druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb05DruidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb05DruidApplication.class, args);
+	}
+
+}
